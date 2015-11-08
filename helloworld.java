863ea248a8bbfa77856a4191556b9696911c45d8
Dang <v571073@tustx002mlvt03p.t-mobile.com>
2015-11-08 12:48:14 -0600

    first commit

---
+++ helloworld.java
@@ -0,0 +1,8 @@
+public class JavaHelloWorld {
+	public static void main (string [] args)
+	{
+		system.out.println("hello");
+		system.out.println("second hello");
+		system.out.println("third hello");
+	}
+}
